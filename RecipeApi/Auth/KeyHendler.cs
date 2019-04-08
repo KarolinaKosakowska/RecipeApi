46c50348fa0d1e9f1a23edb0c476c12ab32330d1
KarolinaKosakowska <karolina.kosakowska.email@gmail.com>
2019-04-08 20:14:00 +0200

    bad

---
+++ RecipeApi/Auth/KeyHendler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using RecipeApi.Helpers;
+using RecipeApi.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Auth
+{
+    public class KeyHandler : AuthorizationHandler<KeyRequirement>
+    {
+        private IHttpContextAccessor contextAccessor;// mozemy czytac po request i response
+        private readonly IApiKeyRepo repo;
+
+        public KeyHandler(IHttpContextAccessor contextAccessor, IApiKeyRepo repo)
+        {
+            this.contextAccessor = contextAccessor;
+            this.repo = repo;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, KeyRequirement requirement)
+        {
+            var roles = repo.GetDictionary();
+            var httpContext = contextAccessor.HttpContext;
+            string key = httpContext.Request.Headers[StaticValues.ApiKey];
+            if (key != null)
+            {
+                string role = roles.ContainsKey(key) ? roles[key] : PolicyEnum.Lack.ToString();//ContainsKey sprawdza czy istnieje taki klucz, jeli jest to go pobeieramy i przypisujemy jego wartość
+                if (role == requirement.Role.ToString())
+                {
+                    context.Succeed(requirement);
+                }
+                else
+                {
+                    httpContext.Response.StatusCode = 401;
+                    httpContext.Response.WriteAsync(StaticValues.AccesDenied);
+                }
+            }
+            else
+            {
+                httpContext.Response.StatusCode = 400;
+                httpContext.Response.WriteAsync(StaticValues.MissingApiKey);
+            }
+            return Task.CompletedTask;
+
+        }
+    }
+}
--- RecipeApi/Startup.cs
+++ RecipeApi/Startup.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RecipeApi.Auth;
+using RecipeApi.Helpers;
 using RecipeApi.Models;
+using RecipeApi.Models.Interfaces;
+using RecipeApi.Models.Repositories;
 
 namespace RecipeApi
 {
@@ -19,12 +24,26 @@ namespace RecipeApi
         public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
+            StaticValues.ConnectionHelper = configuration.GetConnectionString("SQLiteConnection");
         }
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddDbContext<RecipeContext>(o => o.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-            services.AddDbContext<RecipeContext>(o => o.UseSqlite(configuration.GetConnectionString("SQLiteConnection")));
-
+            services.AddDbContext<RecipeContext>(o => o.UseSqlite(StaticValues.ConnectionHelper));
+            services.AddTransient<IApiKeyRepo, ApiKeyRepo>();
+            services.AddTransient<IAuthorizationHandler, KeyHandler>();
+            services.AddHttpContextAccessor();// daje dostęp do contextu
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(PolicyEnum.Admin.ToString(), policy =>
+                    policy.Requirements.Add(new KeyRequirement(PolicyEnum.Admin)));
+                options.AddPolicy(PolicyEnum.User.ToString(), policy =>
+                    policy.Requirements.Add(new KeyRequirement(PolicyEnum.User)));
+                options.AddPolicy(PolicyEnum.Reader.ToString(), policy =>
+                    policy.Requirements.Add(new KeyRequirement(PolicyEnum.Reader)));
+                options.AddPolicy(PolicyEnum.Lack.ToString(), policy =>
+                    policy.Requirements.Add(new KeyRequirement(PolicyEnum.Lack)));
+            });
             services.AddMvcCore()
                 .AddDataAnnotations()
                 .AddJsonFormatters()
@@ -49,6 +68,25 @@ namespace RecipeApi
             {
                 await context.Response.WriteAsync("Nie znaleziono API.");
             });
+            addDefaultKey();
         }
+        private void addDefaultKey()
+        {
+            using (var db = new RecipeContext())
+            {
+                Key key = new Key
+                {
+                    Name = configuration[nameof(key.Name)],
+                    Role = configuration[nameof(key.Role)],
+                    ExpirationDate = new DateTime(2999, 12, 31)
+                };
+                if (db.Keys.Where(a => a.Name == key.Name).Count() == 0)
+                {
+                    db.Keys.Add(key);
+                    db.SaveChanges();
+                }
+            }
+        }
+
     }
 }
